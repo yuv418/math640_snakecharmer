4908507d5711ac9fb61c33ef7972725934dcf131
Ramesh Balaji <rsb203@scarletmail.rutgers.edu>
2024-04-30 22:46:43 -0400

    Easy hint

--- hintgen.py
+++ hintgen.py
@@ -13,12 +13,14 @@ def background(f):
 
 
 def get_hint(word):
+    prompt = f'Imagine that you are writing a crossword puzzle. As the writer of the crossword puzzle, you must come up with a coherent, thoughtful, and clear hint to help the person playing the puzzle guess a word. Use the most common interpretation of the word when writing your hint. Do not interpret words as company names. For example, the word "dane" could be interpreted as "Dane," and a good hint for the word "dane" would be "A native of Denmark." However, the word "stat" could be interpreted as the UNIX command "stat," but this interpretation is too esoteric. Interpreting "stat" as "an abbreviation of the word statistic" is okay. The hint must be at least 6 words and at most 10 words. You cannot use the word in your hint. Do not say anything other than the hint. Please provide me with a hint for the word "{word}."'
+    print(prompt)
     resp = ollama.chat(
         model="llama3",
         messages=[
             {
                 "role": "user",
-                "content": f'Imagine that you are writing a crossword puzzle. As the writer of the crossword puzzle, you must come up with a coherent, thoughtful, and clear hint to help the person playing the puzzle guess the word. The hint must be at least 6 words and at most 10 words. You cannot use the word in your hint. Do not say anything other than the hint. Please provide me with a hint for the word "{word}."',
+                "content": prompt,
             }
         ],
     )
@@ -36,7 +38,7 @@ def handle_file(puz_file):
         puzzle = json.load(pz_fd)
         if not puzzle["generatedHints"]:
             hints = []
-            for word in puzzle["puzzleString"]:
+            for word in puzzle["puzzleWord"]:
                 hints.append(get_hint(word))
 
             puzzle["hints"] = hints
